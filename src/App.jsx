280e215a3f7269d43a74369dca334be5cbf8027f
jcprada05 <jcprada05@gmail.com>
2023-11-30 11:46:52 -0500

    Initial Configuration - Vite React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BreakingBadForm from './components/BreakingBadForm';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <BreakingBadForm />
+    </>
+  );
+}
+
+export default App;
