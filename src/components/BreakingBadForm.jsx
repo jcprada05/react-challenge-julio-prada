1e7f65773b3477ceeb0346aed0ec877b9fa1d780
jcprada05 <jcprada05@gmail.com>
2023-11-30 13:39:08 -0500

    Breaking bad component and styles

--- src/components/BreakingBadForm.jsx
+++ src/components/BreakingBadForm.jsx
@@ -4,19 +4,22 @@ function BreakingBadForm() {
   return (
     <>
       <div className="main-div">
-        <div>
-          <p>Breaking</p>
-          <p>Bad</p>
+        <div className="breaking-bad-div">
+          <p className="breaking-p">Breaking</p>
+          <p className="bad-p">Bad</p>
         </div>
-        <div>
-          <div>
+        <div className="inputs-div">
+          <div className="inputs-div-fname">
             <p>First Name</p>
-            <input placeholder="Breaking" />
+            <input placeholder="Breaking" className="inputs-div-input" />
           </div>
-          <div>
+          <div className="inputs-div-lname">
             <p>Last Name</p>
-            <input placeholder="Bad" />
+            <input placeholder="Bad" className="inputs-div-input" />
           </div>
+        </div>
+        <div className="button-div">
+          <button className="breakify-button">Breakify</button>
         </div>
       </div>
     </>
